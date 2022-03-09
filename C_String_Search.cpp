88002bcdd45209b618e7b85bf361443d79dd14dd
Shamim448 <you@example.com>
2022-03-09 16:33:43 +0600

    modifiyed

--- C_String_Search.cpp
+++ C_String_Search.cpp
@@ -5,25 +5,19 @@ int main()
 {
     string T, P;
     cin >> T >> P;
-    int c =0;
     int size = T.size() - P.size() + 1;
-
     for (int i = 0; i < size; ++i)
     {
         i = T.find(P, i);
         if (i != -1)
         {
             printf("%d\n", i);
-            c++;
-
         }
         else if(i == -1)
         {
             break;
         }
     }
-    if(c == 0){
-        cout << "The output should be empty.";
-    }
+
     return 0;
 }
