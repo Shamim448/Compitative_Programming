67471ca0e2c43bac0b7484a8f6327878178e0054
Shamim448 <you@example.com>
2022-03-09 16:24:48 +0600

    string search

--- C_String_Search.cpp
+++ C_String_Search.cpp
@@ -1,11 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main(){
-string srt1, str2;
-cin >>srt1;
-cin >> str2;
-f
 
-cout <<srt1 << str2;
-return 0;
+int main()
+{
+    string T, P;
+    cin >> T >> P;
+    int c =0;
+    int size = T.size() - P.size() + 1;
+
+    for (int i = 0; i < size; ++i)
+    {
+        i = T.find(P, i);
+        if (i != -1)
+        {
+            printf("%d\n", i);
+            c++;
+
+        }
+        else if(i == -1)
+        {
+            break;
+        }
+    }
+    if(c == 0){
+        cout << "The output should be empty.";
+    }
+    return 0;
 }
