89e9997bbd63acdbcefe7cd7d935eb5729152c3d
Shamim448 <shamim.448@gmail.com>
2022-03-15 16:47:52 +0600

    Prime number generator

--- primeNumber.cpp
+++ primeNumber.cpp
@@ -1,12 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
+vector <int> primeNumber;
 ///prime number check
 bool isPrime( int n)
 {
     for(int i = 2; i < n; i++ )
     {
         if(n % i == 0)
-        return false;
+            return false;
     }
     if(n < 2)
         return false;
@@ -14,14 +15,32 @@ bool isPrime( int n)
         return true;
 }
 
+/// Prime number generator
+void primeNumberGenerator(int L, int R)
+{
+    for(int i = L; i <= R; i++)
+    {
+        if(isPrime(i))
+        {
+            primeNumber.push_back(i);
+        }
+    }
 
+}
 
 
 int main()
 {
-    int n;
+    int L, R;
+    cin >> L >> R;
+    primeNumberGenerator(L, R);
+    for(auto prime: primeNumber){
+        cout << prime << endl;
+    }
+
+    /*int n;
     cin >> n;
     bool primNumber = isPrime(n);
     cout << primNumber << endl;
-    main();
+    main();*/
 }
