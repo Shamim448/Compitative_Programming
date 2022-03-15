1083d47c89f0e93c374508d0272b4489329d1b56
Shamim448 <shamim.448@gmail.com>
2022-03-15 15:18:20 +0600

    prime or not check

---
+++ primeNumber.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+///prime number check
+bool isPrime( int n)
+{
+    for(int i = 2; i < n; i++ )
+    {
+        if(n % i == 0)
+            return false;
+        else if(n < 2)
+            return false;
+        else
+            return true;
+
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    bool primNumber = isPrime(n);
+    cout << primNumber << endl;
+    main();
+    }
