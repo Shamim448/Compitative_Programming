601ba7524168408264faa6d81f6b663e1a69b49f
sad@a.c <ab@ab.com>
2022-04-19 09:00:57 +0600

    big mod try

--- BigMod.cpp
+++ BigMod.cpp
@@ -1,18 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
-int M = 3;
-long long getBigMod(long long B, long long P){
-if(P == 0) return 1;
-if(P % 2 == 0){
-    long long half = getBigMod(B, P/2)% M;
-    return (half*half) % M;
+long long getBigMod(long long B, long long P, long long M)
+{
+    if(P == 0) return 1;
+    if(P % 2 == 0)
+    {
+        long long half = getBigMod(B, P/2, M);
+        return (half*half) % M;
+    }
+    long long x = B%M;
+    long long y = getBigMod(B, P-1, M);
+    return (x * y )% M;
 }
-return (B * getBigMod(B, P-1))% M;
-}
-int main(){
-
-     cout << getBigMod(3, 18132) << endl;
-
-
-return 0;
+int main()
+{
+    long long b, p, m;
+    cin >> b >> p >> m;
+    cout << getBigMod(b, p, m) << endl;
+    return 0;
 }
