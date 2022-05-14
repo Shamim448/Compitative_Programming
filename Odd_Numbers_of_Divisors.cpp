69a530d1edb3339aefcc605849a369339978268b
sad@a.c <ab@ab.com>
2022-05-14 18:44:33 +0600

    deafult divisor count display

--- Odd_Numbers_of_Divisors.cpp
+++ Odd_Numbers_of_Divisors.cpp
@@ -3,44 +3,29 @@ using namespace std;
 int divisorCount(long long N)
 {
     if(N == 1) return 1;
-    int counts = 2;
-    for(int i = 2; i < N; i++)
+    int counts = 0;
+    for(int i = 1; i * i <= N; i++)
     {
         if(N % i == 0)
         {
-            counts++;
+            counts+=1;
+            if(N/i != i){
+            counts+=1;
         }
+        }
+
     }
     return counts;
 }
 int main()
 {
-    int T;
-    cin>> T;
-    while(T--)
-    {
-        long long k,low, high;
-        cin>>k>>low>>high;
-        vector<long long> arr;
-        for(int i= low; i <= high; i++)
-        {
-            if(divisorCount(i) % 2 != 0){
-            arr.push_back(divisorCount(i));
-            }
-        }
-        int j = 1;
-        int counts = 0;
-        for( auto it:arr)
-        {
-            if(it == k)
-            {
-                counts++;
-            }
-             cout << j++ << " = " << it <<endl;
-        }
-        cout << counts << endl;
 
-    }
+        for(int i= 2; i <= 49; i++)
+        {
+            cout << i << " = " << divisorCount(i) << endl;
+        }
+
+
 
 
     return 0;
