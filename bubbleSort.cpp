76e57d724b9ad53926c6a014dab683067a8d8d81
sad@a.c <ab@ab.com>
2022-06-15 19:11:04 +0600

    bubble sort classified

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -9,32 +9,37 @@ void printArray(vector<int> &vec)
         cout << it << " " ;
     }
 }
-
 void bubbleSort(vector<int> &vec)
 {
     int temp;
+    int isSorted = 0;
     for(int i = 0; i < vec.size(); i++) //number of pass
     {
-        for( int j = 0; j < vec.size() - i; j++) //comparison
+        isSorted = 1;
+        for( int j = 0; j < vec.size() - i; j++) //comparison of each pass
         {
             if ( vec[j] > vec[j+1])
             {
                 temp        = vec[j];
                 vec[j]      = vec[j+1];
                 vec[j+1]    = temp;
+                isSorted = 0;
             }
+        }
+        if(isSorted)
+        {
+            return;
         }
     }
 }
 
 int main()
 {
-    vector<int> vec = {12, 54, 65, 7, 23, 9};
+   // vector<int> vec = {12, 54, 65, 7, 23, 9};
+    vector<int> vec = {1,2,3,4,5};
     printArray(vec); /// print original array
     bubbleSort(vec);
     cout<<endl;
     printArray(vec);
-
-
     return 0;
 }
