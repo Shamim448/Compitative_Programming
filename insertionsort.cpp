cc45ab7b15c2784f4b682db5c8bd3c04e5ac89ed
shamim448 <shamim.448@gmail.com>
2022-06-16 18:14:13 +0600

    Insertion sort done

--- insertionsort.cpp
+++ insertionsort.cpp
@@ -1,7 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
+void insertionSort(vector<int> &arr)
+{
+    int j;
+    for(int i = 1; i < arr.size(); i++)
+    {
+        j = i-1;
+        while( j >= 0 & (arr[j] > arr[i]))
+        {
+            swap(arr[j], arr[i]);
+            j--;
+            i--;
+        }
+    }
+}
+/// for print array
+void printArray(vector<int> &arr)
+{
+    for(auto it : arr)
+    {
+        cout << it << " ";
+    }
+    printf("\n");
+}
 
-int main(){
-return 0;
+int main()
+{
+    vector<int> arr = {8556,995,55,6598,15,64,5,44,959,2,4,65,99,15};
+    printArray(arr);
+    insertionSort(arr);
+    printArray(arr);
+    return 0;
 }
 
