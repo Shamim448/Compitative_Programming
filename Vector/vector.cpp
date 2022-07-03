2221bae7b6edcfeaf2eda8ed1512792e68eefaaa
shamim448 <shamim.448@gmail.com>
2022-07-03 09:44:54 +0600

    Add Vector folder for stl

---
+++ Vector/vector.cpp
@@ -0,0 +1,12 @@
+// Vector: This is a part of STL containers
+//It's contain sequential value
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    //initialization
+
+    //insert value in pair
+
+return 0;
+}
