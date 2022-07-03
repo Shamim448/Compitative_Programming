45fde7b065cdba5b328d081f84f51191ca249097
shamim448 <shamim.448@gmail.com>
2022-07-03 09:39:52 +0600

    Add pair folder for stl pair

---
+++ Pair/pair.cpp
@@ -0,0 +1,13 @@
+// Pair: This is a part of STL containers
+//but it's not actually containers
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    //initialization
+    pair <int , string> p;
+    //insert value in pair
+    p = make_pair(2, "Shamim");
+    cout << p.first;
+return 0;
+}
