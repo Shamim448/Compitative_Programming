237b4e07a3b96634a66522ac1ae503d24f4f4be6
shamim448 <shamim.448@gmail.com>
2022-07-03 10:58:09 +0600

    update pair

--- Pair/pair.cpp
+++ Pair/pair.cpp
@@ -1,13 +1,15 @@
 // Pair: This is a part of STL containers
-//but it's not actually containers
+// but it's not actually containers
 
 #include <bits/stdc++.h>
 using namespace std;
-int main(){
-    //initialization
-    pair <int , string> p;
-    //insert value in pair
-    p = make_pair(2, "Shamim");
-    cout << p.first;
-return 0;
+int main()
+{
+    // initialization
+    pair<int, string> p;
+    // insert value in pair
+    // p = make_pair(2, "Shamim");
+    p = {6, "Shohaib"};
+    cout << p.first << " " << p.second;
+    return 0;
 }
