95970f7851e4eb60f57d152b2438bb293c5ed785
Shamim448 <shamim.448@gmail.com>
2022-08-03 22:26:15 +0600

    calculate s2 hash value using generateHash function and calculate power of 10

--- String-Hash/string-search.cpp
+++ String-Hash/string-search.cpp
@@ -1,23 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define BASE 26
-#define MAX_S 10000
-long long h[MAX_S];
-void generatePrefixHash(string &s){
+#define BASE 10
+#define MAX_S 10005
+long long h[MAX_S], po[MAX_S];
+void generatePrefixHash(string &s)
+{
     h[0] = s[0] - 'a' + 1;
-    for(int i = 1; i < s.size(); i++){
+    for(int i = 1; i < s.size(); i++)
+    {
         h[i] = h[i - 1] * BASE + s[i] - 'a' + 1;
     }
-    for(int i = 0; i < s.size(); i++){
-        cout << h[i] << "\n";
-    }
-
-
+    po[0] = 1;
 }
-int main(){
+long long generateHash(string &s2){
+    long long hashValue = 0;
+    for(auto c : s2){
+        hashValue = hashValue * BASE + c - 'a' + 1;
+    }
+    return hashValue;
+}
+
+int main()
+{
     string s;
     cin >> s;
     generatePrefixHash(s);
+    cout << generateHash(s);
     main();
 
     return 0;
