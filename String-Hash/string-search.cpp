2877bd6aabfaa2a9e7a47b13f0a05d083698cc2b
Shamim448 <shamim.448@gmail.com>
2022-08-04 08:53:47 +0600

    create GetHash function for calculate substring hash value with power calculation

--- String-Hash/string-search.cpp
+++ String-Hash/string-search.cpp
@@ -9,23 +9,40 @@ void generatePrefixHash(string &s)
     for(int i = 1; i < s.size(); i++)
     {
         h[i] = h[i - 1] * BASE + s[i] - 'a' + 1;
+        po[0] = 1;
+        for(int i = 1; i < s.size(); i++)
+        {
+            po[i] = po[i - 1] * BASE; /// p[x] = p[x-1] * BASE
+        }
     }
-    po[0] = 1;
 }
-long long generateHash(string &s2){
+long long generateHash(string &s2)
+{
     long long hashValue = 0;
-    for(auto c : s2){
+    for(auto c : s2)
+    {
         hashValue = hashValue * BASE + c - 'a' + 1;
     }
     return hashValue;
+}
+long long getHash(int L, int R)  ///generate hash s[L, R] --> s="abccdabc"--> hash(0, 2) == abc
+{
+    if (L == 0) return h[R];
+    return (h[R] - (h[L-1] * po[R - L + 1]) );  ///h[R] prefix - h[L-1] ar prefix * BASE ^ (R - L +1) substr size
 }
 
 int main()
 {
     string s;
-    cin >> s;
+    string s2;
+    cin >> s >> s2;
     generatePrefixHash(s);
-    cout << generateHash(s);
+   long long hashOfS2 = generateHash(s2);
+   for(int i = 0; i < s.size(); i++){
+    if(getHash(i, i + s2.size() - 1) == hashOfS2){
+        cout << i << "\n";
+    }
+   }
     main();
 
     return 0;
